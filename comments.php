6e2344dd67d5b462863c18f8b7037e0ab2837245
Jonathan Camenisch <jonathan@camenisch.net>
2014-02-05 20:48:02 -0800

    Use Diqus for all comments

--- comments.php
+++ comments.php
@@ -1,59 +1,19 @@
-<?php
-/**
- * The template for displaying Comments
- *
- * The area of the page that contains comments and the comment form.
- *
- * @package WordPress
- * @subpackage Randkcamenisch_Twenty_Thirteen
- * @since R & Camenisch: Twenty Thirteen 0.1
- */
+<? if ( post_password_required() ) return; ?>
 
-/*
- * If the current post is protected by a password and the visitor has not yet
- * entered the password we will return early without loading the comments.
- */
-if ( post_password_required() )
-	return;
-?>
+<div id="disqus_thread"></div>
+<script type="text/javascript">
+  var disqus_shortname = 'kaycamenisch';
+  var disqus_identifier = '<?= the_date("Y-m-d") ?>-<?= basename(get_permalink()) ?>';
+  var disqus_url = '<?= get_permalink() ?>';
 
-<div id="comments" class="comments-area">
-
-	<?php if ( have_comments() ) : ?>
-		<h2 class="comments-title">
-			<?php
-				printf( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'randkcamenisch-twentythirteen' ),
-					number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
-			?>
-		</h2>
-
-		<ol class="comment-list">
-			<?php
-				wp_list_comments( array(
-					'style'       => 'ol',
-					'short_ping'  => true,
-					'avatar_size' => 74,
-				) );
-			?>
-		</ol><!-- .comment-list -->
-
-		<?php
-			// Are there comments to navigate through?
-			if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) :
-		?>
-		<nav class="navigation comment-navigation" role="navigation">
-			<h1 class="screen-reader-text section-heading"><?php _e( 'Comment navigation', 'randkcamenisch-twentythirteen' ); ?></h1>
-			<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'randkcamenisch-twentythirteen' ) ); ?></div>
-			<div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'randkcamenisch-twentythirteen' ) ); ?></div>
-		</nav><!-- .comment-navigation -->
-		<?php endif; // Check for comment navigation ?>
-
-		<?php if ( ! comments_open() && get_comments_number() ) : ?>
-		<p class="no-comments"><?php _e( 'Comments are closed.' , 'randkcamenisch-twentythirteen' ); ?></p>
-		<?php endif; ?>
-
-	<?php endif; // have_comments() ?>
-
-	<?php comment_form(); ?>
-
-</div><!-- #comments -->
+  /* * * DON'T EDIT BELOW THIS LINE * * */
+  (function() {
+    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+    dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+  })();
+</script>
+<noscript>
+  Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a>
+</noscript>
+<a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
