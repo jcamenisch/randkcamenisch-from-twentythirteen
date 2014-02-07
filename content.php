38ce42bbaf88d2e9a1238d1d14d3926cc95f7685
Jonathan Camenisch <jonathan@camenisch.net>
2014-02-07 14:24:59 -0800

    Adjust blog design details

--- content.php
+++ content.php
@@ -11,46 +11,52 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-  <header class="entry-header">
-    <?php if ( has_post_thumbnail() && ! post_password_required() ) : ?>
-    <div class="entry-thumbnail">
-      <?php the_post_thumbnail(); ?>
-    </div>
-    <?php endif; ?>
 
-    <?php if ( is_single() ) : ?>
+  <?php if ( is_single() ) : ?>
+    Subscribe via Email:
+    <form style="display:inline-block;margin-top:-4px;vertical-align:top"
+     action="http://feedburner.google.com/fb/a/mailverify" method="post" target="popupwindow"
+     onsubmit="window.open('http://feedburner.google.com/fb/a/mailverify?uri=kaycamenisch', 'popupwindow', 'scrollbars=yes,width=550,height=520'); return true">
+      <input type="text" style="width:140px" name="email">
+      <input type="hidden" value="kaycamenisch" name="uri"><input type="hidden" name="loc" value="en_US">
+      <button type="submit" value="Subscribe">Go</button>
+    </form>
+    or <a href="http://feeds.feedburner.com/kaycamenisch"><img src="/wp-content/uploads/2014/02/rss-16.png"></a> <a href="http://feeds.feedburner.com/kaycamenisch">RSS</a>
+
+    <hr>
+
     <h1 class="entry-title"><?php the_title(); ?></h1>
-    <?php else : ?>
-    <h1 class="entry-title">
+  <?php else : ?>
+    <h2 class="entry-title">
       <a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
-    </h1>
-    <?php endif; // is_single() ?>
+    </h2>
+  <?php endif; ?>
 
-    <div class="entry-meta">
-      <?php twentythirteen_entry_meta(); ?>
-    </div><!-- .entry-meta -->
-  </header><!-- .entry-header -->
+  <p class="post-meta">posted <?= the_date() ?> <? the_tags('under') ?></p>
 
   <?php if ( is_search() ) : // Only display Excerpts for Search ?>
-  <div class="entry-summary">
-    <?php the_excerpt(); ?>
-  </div><!-- .entry-summary -->
+    <div class="entry-summary">
+      <?php the_excerpt(); ?>
+    </div>
   <?php else : ?>
-  <div class="entry-content">
     <?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'randkcamenisch-twentythirteen' ) ); ?>
-    <?php wp_link_pages( array( 'before' => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'randkcamenisch-twentythirteen' ) . '</span>', 'after' => '</div>', 'link_before' => '<span>', 'link_after' => '</span>' ) ); ?>
-  </div><!-- .entry-content -->
+    <?php wp_link_pages( array(
+            'before' => '<div class="page-links"><span class="page-links-title">' .
+                        __( 'Pages:', 'randkcamenisch-twentythirteen' ) . '</span>',
+            'after' => '</div>',
+            'link_before' => '<span>',
+            'link_after' => '</span>'
+          ) );
+    ?>
   <?php endif; ?>
 
   <footer class="entry-meta">
-    <?php if ( comments_open() && ! is_single() ) : ?>
+    <? if ( comments_open() && ! is_single() ) : ?>
       <div class="comments-link">
-        <?php comments_popup_link( '<span class="leave-reply">' . __( 'Leave a comment', 'randkcamenisch-twentythirteen' ) . '</span>', __( 'One comment so far', 'randkcamenisch-twentythirteen' ), __( 'View all % comments', 'randkcamenisch-twentythirteen' ) ); ?>
-      </div><!-- .comments-link -->
-    <?php endif; // comments_open() ?>
-
-    <?php if ( is_single() && get_the_author_meta( 'description' ) && is_multi_author() ) : ?>
-      <?php get_template_part( 'author-bio' ); ?>
-    <?php endif; ?>
-  </footer><!-- .entry-meta -->
-</article><!-- #post -->
+        <a href="<?php the_permalink(); ?>#disqus_thread" rel="bookmark">Leave a comment</a>
+      </div>
+    <? elseif ( is_single() && get_the_author_meta( 'description' ) && is_multi_author() ) : ?>
+      <? get_template_part( 'author-bio' ); ?>
+    <? endif; ?>
+  </footer>
+</article>
--- single.php
+++ single.php
@@ -140,7 +140,6 @@
       <?php while ( have_posts() ) : the_post(); ?>
 
         <?php get_template_part( 'content', get_post_format() ); ?>
-        <?php twentythirteen_post_nav(); ?>
         <?php comments_template(); ?>
 
       <?php endwhile; ?>
