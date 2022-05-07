9fb37b8c6c9e351b05fe4bc438f5bad15f5929f1
Jonathan Camenisch <jonathan@camenisch.net>
2022-05-07 13:51:30 -0600

    Remove G Analytics from theme
    to inject with plugin instead

--- page.php
+++ page.php
@@ -121,17 +121,5 @@
 
     <? wp_footer(); ?>
 
-    <script type="text/javascript">
-      var _gaq = _gaq || [];
-      _gaq.push(['_setAccount', 'UA-1087558-4']);
-      _gaq.push(['_setDomainName', '.randkcamenisch.com']);
-      _gaq.push(['_trackPageview']);
-      (function(d) {
-        var ga = d.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == d.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = d.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-      })(document);
-    </script>
-
 </body>
 </html>
